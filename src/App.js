f5e3254adf805ed03dd33899acee9c253e97fb6e
Santa Zukovska <zukovska.santa@gmail.com>
2021-03-04 19:43:04 +0200

    Moved content to React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="grid-container">
+      <div class="selectedCurrency currency-1">
+        <button aria-label="Close" class="closeButton">
+          <span aria-hidden="true">×</span>
+        </button>
+        <label>EUR</label>
+      </div>
+      <div class="selectedCurrency currency-2">
+        <button aria-label="Close" class="closeButton">
+          <span aria-hidden="true">×</span>
+        </button>
+        <label>CZK</label>
+      </div>
+      <div class="selectedCurrency currency-3">
+        <button aria-label="Close" class="closeButton">
+          <span aria-hidden="true">×</span>
+        </button>
+        <label>RUB</label>
+      </div>
+      <button class="button">
+        <input type="checkbox" />
+        <label>EUR</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>PLN</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>GEL</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>DKK</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>CZK</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>GBP</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>SEK</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>USD</label>
+      </button>
+      <button href="#" class="button">
+        <input type="checkbox" />
+        <label>RUB</label>
+      </button>
     </div>
   );
 }
